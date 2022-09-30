7b3d33995564728efe4c73f4103131148d21874d
Noriega402 <d.noriegachajon@gmail.com>
2022-09-30 17:51:18 -0600

    Add url with query parameters and endpoints

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-const API = 'https://api.thecatapi.com/v1/images/search';
+const API = 'https://api.thecatapi.com/v1/images/search?limit=10&page=10';
 const btn = document.getElementById('btn-random');
+const parent = document.getElementById('mini');
 
 async function data(url){
     const response =  await fetch(url);
@@ -11,8 +12,16 @@ const peticion = async url => {
     try{
         const getImage =  await data(`${API}`);
         const img = document.getElementById('img');
-        img.setAttribute('src', `${getImage[0].url}`);
-        console.log(getImage);
+        const fragment = document.createDocumentFragment();
+        getImage.forEach(image => {
+            console.log(image);
+            const tag = document.createElement('IMG');
+            tag.setAttribute('src', image.url);
+            tag.setAttribute('alt', image.id);
+            tag.classList.add('img');
+            fragment.append(tag);
+        });
+        parent.appendChild(fragment);
     }catch(error){
         console.log(error);
     }

2cbaf741a790feaaad797b87b4377d27bff9ffd6
Noriega402 <d.noriegachajon@gmail.com>
2022-09-30 14:53:23 -0600

    functions async await and eventlistener of button random

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
-// console.log('Daniel');
-
 const API = 'https://api.thecatapi.com/v1/images/search';
+const btn = document.getElementById('btn-random');
 
-fetch(API)
-.then(response => response.json())
-.then(data => {
-    const img = document.getElementById('img');
-    img.setAttribute('src', `${data[0].url}`);
+async function data(url){
+    const response =  await fetch(url);
+    const data = await response.json();
+    return data;
+}
+
+const peticion = async url => {
+    try{
+        const getImage =  await data(`${API}`);
+        const img = document.getElementById('img');
+        img.setAttribute('src', `${getImage[0].url}`);
+        console.log(getImage);
+    }catch(error){
+        console.log(error);
+    }
+}
+peticion(API);
+
+btn.addEventListener('click', () => {
+    peticion(API);
 })
